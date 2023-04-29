73fddc714dc2b71d9c1798f6078dd3239254dca9
rafaguima00 <rafael16gsm@gmail.com>
2023-04-29 19:37:57 -0300

    Carregamento de dados e tratamento de erros

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
 const urlApi = 'https://rickandmortyapi.com/api/character'
 const lista = document.getElementById('mostrar-dados')
-const btPrev = document.getElementById('botao-prev')
-const btNext = document.getElementById('botao-next')
-const form = document.getElementById('form-buscar')
+const mensagemNaTela = document.createElement('p')
 
 let botaoPrev = ''
 let botaoNext = ''
 
 async function recuperarDados(url, name = '') {
+    
+    mensagemNaTela.innerText = "Carregando dados dos personagens..."
+    mensagemNaTela.classList.add('mensagem-tela')
+    
+    const main = document.querySelector('main')
+    main.insertBefore(mensagemNaTela, main.children[1])
 
-    if(name != '') {
-        var recuperarUrl = await fetch(`${url}?name=${name}`)
-    } else {
-        var recuperarUrl = await fetch(url)
+    try {
+        if(name != '') {
+            var recuperarUrl = await fetch(`${url}?name=${name}`)
+        } else {
+            var recuperarUrl = await fetch(url)
+        }
+
+        var urlConvertida = await recuperarUrl.json()
+
+        botaoPrev = urlConvertida.info.prev
+        botaoNext = urlConvertida.info.next
+
+        const arrayItens = urlConvertida.results
+
+        arrayItens.forEach((elemento) => {
+            mostrarDados(elemento)
+        })
+
+        mensagemNaTela.remove()
+
+    } catch (e) {
+        mensagemNaTela.innerText = "Nenhum registro"
+        botaoNext = null
     }
-
-    var urlConvertida = await recuperarUrl.json()
-
-    botaoPrev = urlConvertida.info.prev
-    botaoNext = urlConvertida.info.next
-
-    const arrayItens = urlConvertida.results
-
-    arrayItens.forEach((elemento) => {
-        mostrarDados(elemento)
-    })
 
     return urlConvertida
 }
 
+const form = document.getElementById('form-buscar')
 form.onclick = function(evento) {
     const inputForm = document.getElementById('input-buscar')
 
@@ -67,6 +80,7 @@ function mostrarDados(itens) {
     `
 }
 
+const btPrev = document.getElementById('botao-prev')
 btPrev.addEventListener('click', (evento) => {
     evento.preventDefault()
 
@@ -78,6 +92,7 @@ btPrev.addEventListener('click', (evento) => {
     }
 })
 
+const btNext = document.getElementById('botao-next')
 btNext.addEventListener('click', (evento) => {
     evento.preventDefault()
 
