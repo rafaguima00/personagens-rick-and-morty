4b6f02c3c62bc36601ae51826ea84a57d15f1014
rafaguima00 <rafael16gsm@gmail.com>
2023-11-14 10:14:45 -0300

    Atualização do commit

--- js/lastUpdate.js
+++ js/lastUpdate.js
@@ -1,3 +1,14 @@
-const dateTime = document.querySelector("#date-time")
+const dateTime = document.querySelector("#date-time");
+const url = "https://api.github.com/users/rafaguima00/repos";
 
-dateTime.innerHTML = new Date().toLocaleString()
+const getUrl = async () => {
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    try {
+        dateTime.innerHTML += new Date(responseJson[6].pushed_at).toLocaleString();
+    } catch (error) {
+        dateTime.innerHTML += "Nenhuma atualização"
+    }
+}
+
+getUrl();
